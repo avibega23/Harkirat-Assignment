80576d148b7652727434f26c054f9e831009ff0c
Avinoor Singh <avinoor196@gmail.com>
2025-06-23 16:20:56 +0530

    update

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
-
+mongoose.connect('mongodb+srv://avinoor196:avibega123@cluster0.hkowdvt.mongodb.net/');
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    password:String,
 });
+
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    password:String,
+    purchasedCourses:[{
+        type: mongoose.Types.ObjectId,
+        ref: "course"
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title:String,
+    description:String,
+    imageLink:String,
+    price: Number
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
