d12c10ad06b593eb9bd605fda99cc75fd6771df3
Avinoor Singh <avinoor196@gmail.com>
2025-06-23 18:51:01 +0530

    week 3 task03 done

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,47 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const username = req.headers.username;
+  const password = req.headers.password;
+
+  const val = await Admin.findOne({ username: username, password: password });
+  if (val) {
+    res.status(403).json({ msg: "Admin Already Exist" });
+    return;
+  }
+  Admin.create({ username: username, password: password });
+  res.json({ msg: "Admin Created Successfully" });
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, async (req, res) => {
+  // Implement course creation logic
+  const username = req.headers.username;
+  const password = req.headers.password;
+  const title = req.body.title;
+  const description = req.body.description;
+  const price = req.body.price;
+  const imageLink = req.body.imageLink;
+
+  const newCourse = await Course.create({
+    username: username,
+    password: password,
+    title: title,
+    price: price,
+    description: description,
+    imageLink: imageLink,
+  })
+  res.json({ msg: "Course Created Succesfully", id: newCourse._id });
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware,async (req, res) => {
+  // Implement fetching all courses logic
+  const courses = await Course.find({})
+  res.json({courses:courses})
 });
 
 module.exports = router;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,54 @@
 const { Router } = require("express");
 const router = Router();
+const { User, Course } = require("../db/index");
 const userMiddleware = require("../middleware/user");
+const mongoose = require("mongoose");
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", async (req, res) => {
+  // Implement user signup logic
+  const username = req.headers.username;
+  const password = req.headers.password;
+
+  const val = await User.findOne({ username: username, password: password });
+  if (val) {
+    res.status(403).json({ msg: "Admin Already Exist" });
+    return;
+  }
+  User.create({ username: username, password: password });
+  res.json({ msg: "User Created Successfully" });
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  const courses = await Course.find({});
+  res.json({ courses: courses });
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+  const username = req.headers.username;
+  const id = req.params.courseId;
+
+  await User.updateOne(
+    { username: username },
+    { $push: { purchasedCourses: id } }
+  )
+  res.json({ msg: "purchase complete" });
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware,async (req, res) => {
+  // Implement fetching purchased courses logic
+  const username =  req.headers.username;
+  const user = await User.findOne({username:username})
+  const arr = user.purchasedCourses
+  list = []
+  for(let i = 0;i<arr.length;i++)
+  {
+    const course = await Course.findById(arr[i])
+    list.push(course)
+  }
+  res.json({courses:list})
 });
 
-module.exports = router
+module.exports = router;
